281fc69ccbb1daaec45b2ca5e1e924570f610b63
Esteban <esteban.aleart@gmail.com>
2024-10-16 18:51:01 -0300

    saco municipios

--- src/app/components/fetching/fetchingMunicipios.js
+++ src/app/components/fetching/fetchingMunicipios.js
@@ -1,24 +1,24 @@
-"use client"
+// "use client"
 
-import axios from 'axios';
-import {  useState } from 'react';
+// import axios from 'axios';
+// import {  useState } from 'react';
 
-// Componente para filtrar municipios por provincia
-export default async function fetchMunicipios({ provinciaId }) {
-  const [municipios, setMunicipios] = useState([]);
+// // Componente para filtrar municipios por provincia
+// export default async function fetchMunicipios({ provinciaId }) {
+//   const [municipios, setMunicipios] = useState([]);
 
  
   
-    try {
-      const response = await axios.get('https://infra.datos.gob.ar/catalog/modernizacion/dataset/7/distribution/7.4/download/municipios.json');
+//     try {
+//       const response = await axios.get('https://infra.datos.gob.ar/catalog/modernizacion/dataset/7/distribution/7.4/download/municipios.json');
       
      
-      const municipiosFiltrados = response.data.municipios.filter(municipio => Number(municipio.provincia.id) === Number(provinciaId));
+//       const municipiosFiltrados = response.data.municipios.filter(municipio => Number(municipio.provincia.id) === Number(provinciaId));
       
      
-      setMunicipios(municipiosFiltrados);
-      if (municipios.length) return municipios
-    } catch (error) {
-      console.error('Error al obtener los municipios:', error);
-    }
-}
+//       setMunicipios(municipiosFiltrados);
+//       if (municipios.length) return municipios
+//     } catch (error) {
+//       console.error('Error al obtener los municipios:', error);
+//     }
+// }
